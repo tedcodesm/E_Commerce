7e7d85c8ec2feeb5039d25721c67a27d8f9d6b64
tedcodesm <kaahenjoroge@gmail.com>
2025-04-11 15:08:22 +0300

    ksh

--- frontend/src/components/FeaturedProduct.jsx
+++ frontend/src/components/FeaturedProduct.jsx
@@ -55,7 +55,7 @@ const FeaturedProducts = ({ featuredProducts }) => {
 										<div className='p-4'>
 											<h3 className='text-lg font-semibold mb-2 text-white'>{product.name}</h3>
 											<p className='text-emerald-300 font-medium mb-4'>
-												${product.price.toFixed(2)}
+												ksh{product.price.toFixed(2)}
 											</p>
 											<button
 												onClick={() => addToCart(product)}
--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -65,13 +65,13 @@ const OrderSummary = () => {
 				<div className='space-y-2'>
 					<dl className='flex items-center justify-between gap-4'>
 						<dt className='text-base font-normal text-gray-300'>Original price</dt>
-						<dd className='text-base font-medium text-white'>${formattedSubtotal}</dd>
+						<dd className='text-base font-medium text-white'>ksh{formattedSubtotal}</dd>
 					</dl>
 
 					{savings > 0 && (
 						<dl className='flex items-center justify-between gap-4'>
 							<dt className='text-base font-normal text-gray-300'>Savings</dt>
-							<dd className='text-base font-medium text-emerald-400'>-${formattedSavings}</dd>
+							<dd className='text-base font-medium text-emerald-400'>-ksh{formattedSavings}</dd>
 						</dl>
 					)}
 
@@ -83,7 +83,7 @@ const OrderSummary = () => {
 					)}
 					<dl className='flex items-center justify-between gap-4 border-t border-gray-600 pt-2'>
 						<dt className='text-base font-bold text-white'>Total</dt>
-						<dd className='text-base font-bold text-emerald-400'>${formattedTotal}</dd>
+						<dd className='text-base font-bold text-emerald-400'>ksh{formattedTotal}</dd>
 					</dl>
 				</div>
 
