294d679a0a8e81d315634c60d0a23c372176171a
tedcodesm <kaahenjoroge@gmail.com>
2025-04-17 12:48:30 +0300

    localerror

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -42,10 +42,10 @@ export const createPesapalPaymentSession = async (req, res) => {
     }
 
     const orderRef = crypto.randomUUID();
-    const ipnNotificationUrl ="https://3a76-41-209-60-99.ngrok-free.app/api/payment/pesapal-ipn";
-    // const ipnNotificationUrl = "https://ecommerce-vtt3.onrender.com/api/payment/pesapal-ipn";
-    const callbackurl = `https://3a76-41-209-60-99.ngrok-free.app/api/payments/pesapal-callback?orderRef=${orderRef}&email=${req.user.email}`;
-    // const callbackurl = `https://ecommerce-vtt3.onrender.com/api/payments/pesapal-callback?orderRef=${orderRef}`
+    // const ipnNotificationUrl ="https://3a76-41-209-60-99.ngrok-free.app/api/payment/pesapal-ipn";
+    const ipnNotificationUrl = "https://ecommerce-vtt3.onrender.com/api/payment/pesapal-ipn";
+    // const callbackurl = `https://3a76-41-209-60-99.ngrok-free.app/api/payments/pesapal-callback?orderRef=${orderRef}&email=${req.user.email}`;
+    const callbackurl = `https://ecommerce-vtt3.onrender.com/api/payments/pesapal-callback?orderRef=${orderRef}&email=${req.user.email}`
 
     const getAccessToken = async (req, res) => {
       console.log("Token has been requested");
